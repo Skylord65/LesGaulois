2bbb0faf81acc55c738e0094eb6d5a82e8d90a67
Skylord65 <mloann2802@gmail.com>
2024-02-15 11:45:52 +0100

    Classe Gaulois

---
+++ src/personnages/Gaulois.java
@@ -0,0 +1,35 @@
+package personnages;
+
+import java.io.ObjectInputStream.GetField;
+
+public class Gaulois {
+	private String nom;
+	private int force;
+	private int effetPotion = 1;
+
+	public Gaulois(String nom, int force) {
+		this.nom = nom;
+		this.force = force;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	@Override
+	public String toString() {
+		return "Gaulois [nom=" + nom + ", force=" + force + ", effetPotion=" + effetPotion + "]";
+	}
+
+	public static void main(String[] args) {
+		
+	}
+	
+	public void parler() {
+		String parole = prendreParole();
+	}
+	private String prendreParole() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
