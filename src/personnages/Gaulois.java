2535f9eb822e87331616e7f5e08f7beaa8737e4f
Skylord65 <mloann2802@gmail.com>
2024-02-23 00:30:26 +0100

    Fin du TP1 maison

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -15,21 +15,39 @@ public class Gaulois {
 	public String getNom() {
 		return nom;
 	}
-
+	public void parler(String texte) {
+		System.out.println(prendreParole() + "<< " + texte + " >>");
+	}
+	
+	private String prendreParole() {
+		return "Le gaulois " + nom + " : ";
+	}
+	
+	public void frapper(Romain romain) {
+		System.out.println(nom + " envoie un grand coup dans la mâchoire de " 
+				+ romain.getNom());
+		romain.recevoirCoup((force / 3)*effetPotion);
+	}
+		
+	public void boirePotion(int effetPotion) {
+		this.effetPotion = effetPotion;
+		parler("Merci Druide, je sens que ma force est " + effetPotion + " fois décuplée.");
+	}
+	
 	@Override
 	public String toString() {
-		return "Gaulois [nom=" + nom + ", force=" + force + ", effetPotion=" + effetPotion + "]";
+		return "Gaulois [nom=" + nom + ", force=" + force 
+				+ ", effetPotion=" + effetPotion + "]";
 	}
 
 	public static void main(String[] args) {
-		
+//		Gaulois asterix = new Gaulois("Astérix", 8);
+//		Romain brutus = new Romain("Brutus", 8);
+//		System.out.println(asterix.getNom());
+//		asterix.prendreParole();
+//		asterix.parler("hello");
+//		asterix.frapper(brutus);
+//		asterix.boirePotion(2);
 	}
-	
-	public void parler() {
-		String parole = prendreParole();
-	}
-	private String prendreParole() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+
 }
