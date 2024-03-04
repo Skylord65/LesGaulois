7aca8dd0c3c8ee47457cf70879be4bd3a0df56d5
Skylord65 <mloann2802@gmail.com>
2024-03-04 17:12:27 +0100

    Mise à jour de la classe Village : gestion des villageois

--- src/personnages/Village.java
+++ src/personnages/Village.java
@@ -28,4 +28,26 @@ public class Village {
 	public Gaulois trouverHabitant(int numeroVillageois) {
 		return Villageois[numeroVillageois];
 	}
+	
+	public void afficherVillageois() {
+		System.out.println("Dans le village du chef " + chef + " vivent les légendaires gaulois :");
+		for (int i=0; i<nbVillageois; i++ ) {
+			System.out.println("- " + Villageois[i].getNom());
+		}
+	}
+	public static void main(String[] args) {
+		Village village = new Village("Village des Irréductible", 30);
+		//Gaulois gaulois = village.trouverHabitant(30);
+		// Les numéros sont de 0 à 29 donc 30 n'est pas dans le tableau
+		Chef abraracourcix = new Chef("Abraracourcix", 6, village);
+		village.setChef(abraracourcix);
+		Gaulois astérix = new Gaulois("Astérix", 8);
+		village.ajouterHabitant(astérix);
+		//Gaulois gaulois = village.trouverHabitant(1);
+		//System.out.println(gaulois);
+		//null
+		Gaulois obelix = new Gaulois("Obélix", 25);
+		village.ajouterHabitant(obelix);
+		village.afficherVillageois();
+	}
 }
