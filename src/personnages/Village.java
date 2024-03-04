3c6ab4bce86af1d61341a35bc77a95e6cd89aadb
Skylord65 <mloann2802@gmail.com>
2024-03-04 15:38:07 +0100

    Début TP2 : création des villageois

--- src/personnages/Village.java
+++ src/personnages/Village.java
@@ -3,16 +3,29 @@ package personnages;
 public class Village {
 	private String nom;
 	private Chef chef;
+	private Gaulois Villageois[];
+	private int nbVillageois = 0;
 	
-	public Village(String nom) {
+	public Village(String nom, int nbVillageoisMaximum) {
 		this.nom = nom;
+		this.Villageois = new Gaulois [nbVillageoisMaximum];
 	}
 	
 	public void setChef(Chef chef) {
-		this.chef= chef;
+		this.chef = chef;
 	}
 	
 	public String getNom() {
 		return nom;
+	}
+	
+	public void ajouterHabitant(Gaulois gaulois) {
+		Villageois[nbVillageois] = gaulois;
+		nbVillageois += 1;
+		
+	}
+	
+	public Gaulois trouverHabitant(int numeroVillageois) {
+		return Villageois[numeroVillageois];
 	}
 }
