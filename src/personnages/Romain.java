925402b3b9e7049782cc63c7777fa130318a8bd0
Skylord65 <mloann2802@gmail.com>
2024-03-05 08:22:50 +0100

    TP2 : Ajout des assertions des Romains

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -8,6 +8,7 @@ public class Romain {
 	public Romain(String nom, int force) {
 		this.nom = nom;
 		this.force = force;
+		assert force > 0;
 	}
 
 	public String getNom() {
@@ -23,12 +24,15 @@ public class Romain {
 	}
 
 	public void recevoirCoup(int forceCoup) {
+		assert force > 0 : "la force d'un Romain est positive";
+		int forceInit = force;
 		force -= forceCoup;
 		if (force > 0 ) {
 			parler("Aie");
 		} else {
 			parler("J'abandonne...");
 		}
+		assert force < forceInit : "la force d'un a diminué";
 	}
 	
 	public static void main(String[] args) {
@@ -36,5 +40,6 @@ public class Romain {
 //		brutus.prendreParole();
 //		brutus.parler("bonjour");
 //		brutus.recevoirCoup(400);
+//		Romain minus = new Romain("Minus", -6);
 	}
 }
